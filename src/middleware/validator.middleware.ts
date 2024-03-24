fc5da27cf1045d2a51f26855349aa44876dd8c15
ShodiyevF <fayzulloh6757@gmail.com>
2024-03-24 19:48:20 +0500

    add internalErrorCatcher

--- src/middleware/validator.middleware.ts
+++ src/middleware/validator.middleware.ts
@@ -1,5 +1,5 @@
 import { internalErrorCatcher } from "@logger/logger.internal";
-import { DTO, validator } from "shared/validation/validation";
+import { DTO, validator } from "@shared/validation/validation";
 import { Request, Response, NextFunction } from "express";
 import { returnResponse } from "@lib/express.function";
 import { Errors } from "@lib/httpException";
@@ -10,11 +10,15 @@ interface CustomRequest extends Request {
 
 export default function validationMiddleware(dto: DTO, value: string ) {
     return (req: CustomRequest, res: Response, next: NextFunction) => {
-        const validatorResponse = validator(dto, req[value]);
-        if (validatorResponse.status == 200) {
-            return next();
-        } else {
-            return returnResponse(res, 403, validatorResponse.error || 'error', Errors.VALIDATION_ERROR);
+        try {
+            const validatorResponse = validator(dto, req[value]);
+            if (validatorResponse.status == 200) {
+                return next();
+            } else {
+                return returnResponse(res, 403, validatorResponse.error || 'error', Errors.VALIDATION_ERROR);
+            }
+        } catch (error) {
+            internalErrorCatcher(error)
         }
     };
 }
