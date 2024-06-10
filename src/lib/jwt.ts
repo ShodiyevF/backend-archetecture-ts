cd420104a771296d5ebfeea8ab4b7b62e4683338
Fayzulloh <fayzulloh6757@gmail.com>
2024-06-10 14:30:27 +0500

    prefix jwt

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,28 +1,33 @@
+import jwt from 'jsonwebtoken'
+
 import internalErrorCatcher from '@shared/logger/logger.internal';
-import jwt, { JwtPayload } from 'jsonwebtoken'
 
+namespace JWT {
 
-export function requestJwtToken(payload: JwtPayload) {
-    try {
-        const token = jwt.sign({ id: payload }, process.env.JWT_SECRET || 'test', {
-            expiresIn: process.env.JWT_EXPIRATION,
-        });
-        return token;
-    } catch (error) {
-        internalErrorCatcher(error);
-    }
-}
-
-export function verifyJwtToken(token: string) {
-    try {
-        const verifed = jwt.verify(token, process.env.JWT_SECRET || 'test');
-        return verifed;
-    } catch (error: any) {
-        if (error.expiredAt) {
-            return {
-                status: 402,
-            };
+    export function requestJwtToken(payload: jwt.JwtPayload) {
+        try {
+            const token = jwt.sign({ id: payload }, process.env.JWT_SECRET || 'test', {
+                expiresIn: process.env.JWT_EXPIRATION,
+            });
+            return token;
+        } catch (error) {
+            internalErrorCatcher(error);
         }
     }
+    
+    export function verifyJwtToken(token: string) {
+        try {
+            const verifed = jwt.verify(token, process.env.JWT_SECRET || 'test');
+            return verifed;
+        } catch (error: any) {
+            if (error.expiredAt) {
+                return {
+                    status: 402,
+                };
+            }
+        }
+    }
+    
 }
 
+export default JWT
