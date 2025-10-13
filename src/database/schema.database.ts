514dfb05a7221e0114c4f8fd20a0386babf34ea4
Fayzulloh <fayzulloh6757@gmail.com>
2025-10-13 16:03:12 +0500

    refactor schema.database

--- src/database/schema.database.ts
+++ src/database/schema.database.ts
@@ -1,40 +1,51 @@
-import { json, pgTable, smallint, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { json, pgEnum, pgTable, smallint, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 import { Table } from "drizzle-orm";
 
-const requestTypes: [string, ...string[]] = [ "SUCCESS", "ERROR" ]
+export const requestLogsRlTypeEnum = pgEnum('request_logs_rl_type_enum', [ "SUCCESS", "ERROR" ]);
 
 export const requestsLOGSTable = pgTable('requests_logs', {
-    requestId: uuid('request_id').defaultRandom().primaryKey(),
-    requestType: varchar('request_type', { enum: requestTypes }).notNull(),
-    requestMethod: varchar('request_method', { length: 32 }).notNull(),
-    requestRoute: text('request_route').notNull(),
-    requestHost: text('request_host').notNull(),
-    requestUserAgent: text('request_user_agent').notNull(),
-    requestBody: json('request_body').notNull(),
-    requestResponseStatus: smallint('request_response_status').notNull(),
-    requestResponseBody: text('request_response_body').notNull(),
-    requestCreatedAt: timestamp('request_created_at').notNull().defaultNow(),
+    rlId: uuid('rl_id').defaultRandom().primaryKey(),
+    rlType: requestLogsRlTypeEnum('rl_type').notNull(),
+    rlMethod: varchar('rl_method', { length: 32 }).notNull(),
+    rlRoute: text('rl_route').notNull(),
+    rlHost: text('rl_host').notNull(),
+    rlUserAgent: text('rl_user_agent').notNull(),
+    rlBody: json('rl_body').notNull(),
+    rlResponseStatus: smallint('rl_response_status').notNull(),
+    rlResponseBody: text('rl_response_body').notNull(),
+    rlCreatedAt: timestamp('rl_created_at').notNull().defaultNow(),
 })
 
 export const internalErrorsLOGSTable = pgTable('internal_errors_logs', {
-    ieId: uuid('ie_id').defaultRandom().primaryKey(),
-    ieDescription: text('ie_description').notNull(),
-    ieStack: text('ie_stack').notNull(),
-    ieCreatedAt: timestamp('ie_created_at').notNull().defaultNow(),
+    ielId: uuid('iel_id').defaultRandom().primaryKey(),
+    ielDescription: text('iel_description').notNull(),
+    ielStack: text('iel_stack').notNull(),
+    ielCreatedAt: timestamp('iel_created_at').notNull().defaultNow(),
 })
 
 export const cronJobsLOGSTable = pgTable('cron_jobs_logs', {
-    cjId: uuid('cj_id').defaultRandom().primaryKey(),
-    cjName: text('cj_name').notNull(),
-    cjCreatedAt: timestamp('cj_created_at').notNull().defaultNow(),
+    cjlId: uuid('cjl_id').defaultRandom().primaryKey(),
+    cjlName: text('cjl_name').notNull(),
+    cjlCreatedAt: timestamp('cjl_created_at').notNull().defaultNow(),
+})
+
+export const testTable = pgTable('test', {
+    testId: uuid('test_id').defaultRandom().primaryKey(),
+    testName: text('test_name').notNull(),
+    testCreatedAt: timestamp('test_created_at').notNull().defaultNow(),
 })
 
 namespace DbTableSchema {
-    export const requests = requestsLOGSTable
-    export const internalErrors = internalErrorsLOGSTable
-    export const cronJobs = cronJobsLOGSTable
+    export const requestsLOGS = requestsLOGSTable
+    export const internalErrorsLOGS = internalErrorsLOGSTable
+    export const cronJobsLOGS = cronJobsLOGSTable
+    export const test = testTable
 
-    export type InferSelectType<T extends Table> = T['_']['inferSelect'];
+    export const requestLogsRlTypeEnumList = requestLogsRlTypeEnum.enumValues
+    
+    export type TRequestLogsRlTypeEnum = typeof requestLogsRlTypeEnum.enumValues[number]
+
+    export type InferSelectType<T extends Table, P extends boolean | null = null> = P extends true ? Partial<T['_']['inferSelect']> : T['_']['inferSelect'];
     export type InferInsertType<T extends Table> = T['_']['inferInsert'];
     export type InferUpdateType<T extends Table> = Partial<InferInsertType<T>>;
 }
