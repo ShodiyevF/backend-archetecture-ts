560fe7a7274532125cd5e2fc6412491665facc0e
Fayzulloh <fayzulloh6757@gmail.com>
2025-10-16 12:12:12 +0500

    add env variables

---
+++ src/types/env_variables.d.ts
@@ -0,0 +1,14 @@
+export type EnvVariables =
+  | "PORT"
+  | "MODE"
+  | "DOMAIN"
+  | "PROJECT_NAME"
+  | "JWT_SECRET"
+  | "JWT_EXPIRATION"
+  | "ORIGIN"
+  | "CREDENTIALS"
+  | "DB_USER"
+  | "DB_PASSWORD"
+  | "DB_NAME"
+  | "DB_HOST"
+  | "DB_PORT";
