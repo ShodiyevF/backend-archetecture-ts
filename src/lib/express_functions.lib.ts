7a9838abfa45ccbbbac5bd1bef69045969203be5
Fayzulloh <fayzulloh6757@gmail.com>
2025-10-20 10:40:33 +0500

    change controller error

--- src/lib/express_functions.lib.ts
+++ src/lib/express_functions.lib.ts
@@ -31,6 +31,15 @@ namespace ExpressFunctions  {
             next();
         });
     }
+    
+    export function controllerError(res: express.Response, error: any) {
+        if (EnvLib.getVariable('MODE') == 'DEV' && !error.status) {
+            console.log(error)
+            return ExpressFunctions.returnResponse(res, 500, `INTERNAL ERROR: ${error}`, Exception.Errors.INTERNAL_SERVER_ERROR)
+        } else {
+            return ExpressFunctions.returnResponse(res, error.status, error.message, error.error)
+        }
+    }
 }
 
 export default ExpressFunctions
