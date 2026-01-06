0a116adb173f5747c0c58decc864b4b13fee4ae2
Fayzulloh <fayzulloh6757@gmail.com>
2026-01-06 21:40:23 +0500

    add jwt keys

--- src/config/fs.config.ts
+++ src/config/fs.config.ts
@@ -1,16 +1,45 @@
+import childProcess from 'child_process'
 import path from 'path'
 import fs from 'fs'
 
 namespace FS {
+    export namespace StaticPaths {
+        export const uploadsFolder = path.join(process.cwd(), '/upload')
+
+        export const accessPrivateKey = path.join(process.cwd(), '/src/keys/access.private.key')
+
+        export const accessPublicKey = path.join(process.cwd(), '/src/keys/access.public.key')
+    }
+    
     export function initDefaultFolders() {
-        const UPLOAD_FOLDER: boolean = fs.existsSync(path.join(process.cwd(), '/upload'));
+        const UPLOAD_FOLDER = fs.existsSync(StaticPaths.uploadsFolder);
         if (!UPLOAD_FOLDER) {
-            fs.mkdirSync(path.join(process.cwd(), '/upload'));
+            fs.mkdirSync(StaticPaths.uploadsFolder);
+        }
+    }
+    
+    export function initDefaultFiles() {
+        const privateKeyPath = path.join(StaticPaths.accessPrivateKey)
+        const publicKeyPath = path.join(StaticPaths.accessPublicKey)
+        
+        const PRIVATE_KEY = fs.existsSync(privateKeyPath);
+        if (!PRIVATE_KEY) {
+            childProcess.execSync(`openssl genrsa -out ${privateKeyPath} 4096`, {
+                stdio: 'ignore'
+            })
+        }
+
+        const PUBLIC_KEY = fs.existsSync(publicKeyPath);
+        if (!PUBLIC_KEY) {
+            childProcess.execSync(`openssl rsa -in ${privateKeyPath} -pubout -out ${publicKeyPath}`, {
+                stdio: 'ignore'
+            })
         }
     }
 
     export function runner() {
         initDefaultFolders()
+        initDefaultFiles()
     }
 }
 
