8b43c2b93975c3997fa52a6a5885d0c3633f46a7
Fayzulloh <fayzulloh6757@gmail.com>
2026-03-14 17:02:13 +0500

    add cron job for database backup

--- src/config/cronjobs.config.ts
+++ src/config/cronjobs.config.ts
@@ -1,4 +1,8 @@
+import DatabaseBackup from "@database/backup.database"
+import newCronJob from "@lib/cronjob.lib"
+
 function runConfigCronJobs() {
+    newCronJob('get_backup', '* * * * *', DatabaseBackup.getBackup)
 }
 
 export default runConfigCronJobs
